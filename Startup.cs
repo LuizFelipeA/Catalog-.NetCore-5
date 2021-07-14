af479ba03aab237c5964311b872a1e1321046bf0
Luiz Felipe <luiz.felipe.professional@hotmail.com>
2021-07-13 21:29:13 -0300

    Adding asynchronous stream

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,10 @@ namespace Catalog
             });
             services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
             
-            services.AddControllers();
+            services.AddControllers(option => 
+            {
+                option.SuppressAsyncSuffixInActionNames = false;
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
